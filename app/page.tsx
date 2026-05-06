18b248bfe408b27cb1ab4330b75757890bccfe05
Austin Zheng <austin.zheng26@gmail.com>
2026-05-06 18:19:10 -0400

    Completed sales chart
    The sales chart visualizes sales data over time.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
 import TopProducts from "../components/top_products";
 import Summary from "../components/summary";
+import SalesChart from "../components/sales_chart";
+
+const sales_data = [
+  {date: "May 1", revenue: 100},
+  {date: "May 2", revenue: 130},
+  {date: "May 3", revenue: 200},
+  {date: "May 4", revenue: 180},
+  {date: "May 5", revenue: 270},
+];
 
 const products = [
   {name: "Socks", revenue: 230, orders: 25},
-  {name: "Shirt", revenue: 300, orders: 27},
-  {name: "Hat", revenue: 180, orders: 12},
+  {name: "Shirts", revenue: 300, orders: 27},
+  {name: "Hats", revenue: 180, orders: 12},
   {name: "Pants", revenue: 290, orders: 23},
   {name: "Gloves", revenue: 150, orders: 15},
-  {name: "Coat", revenue: 410, orders: 22}
+  {name: "Coats", revenue: 410, orders: 22}
 ];
 
 const byRevenue = [...products];
@@ -19,17 +28,26 @@ byOrders.sort((a,b) => b.orders - a.orders);
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-white font-sans dark:bg-black">
-      <h1 className="max-w-xs text-3xl leading-50 font-bold text-black dark:text-white">
+      <h1 className="max-w-xs text-3xl mt-20 font-bold text-black dark:text-white">
         Analytics Dashboard
       </h1>
 
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-15 px-16 bg-white dark:bg-black sm:items-start">
         <div className="flex flex-col items-center w-full bg-zinc-400 dark:bg-gray-700 p-5 rounded-3xl mb-10">
           <h1 className="max-w-xs text-2xl font-semibold text-black dark:text-white mb-5">
             Summary
           </h1>
 
           <Summary className="w-full bg-zinc-300 dark:bg-gray-600 p-3 rounded-3xl" products={products}/>
+        </div>
+
+
+        <div className="flex flex-col items-center w-full bg-zinc-400 dark:bg-gray-700 p-5 rounded-3xl mb-10">
+          <h1 className="max-w-xs text-2xl font-semibold text-black dark:text-white mb-5">
+            Sales Chart
+          </h1>
+
+          <SalesChart className="w-full bg-zinc-300 dark:bg-gray-600 p-3 rounded-3xl" data={sales_data}/>
         </div>
         
         
---
+++ components/sales_chart.js
@@ -0,0 +1,15 @@
+"use client";
+
+import {LineChart, Line, XAxis, YAxis} from "recharts";
+
+export default function SalesChart({data, className}) {    
+    return (
+        <div className={className}>
+            <LineChart width="95%" height="100%" data={data} margin={{top: 20, right: 20, left: 20, bottom: 20}} responsive>
+                <XAxis dataKey="date" stroke="var(--color-text)" label={{value: "Date", position: "insideBottom", offset: -15, fill: "var(--color-text)"}}/>
+                <YAxis stroke="var(--color-text)" label={{value: "Revenue", position: "insideLeft", angle: -90, fill: "var(--color-text)", dy: 35}}/>
+                <Line type="monotone" dataKey="revenue" stroke="var(--color-chart-line)"/>
+            </LineChart>
+        </div>
+    );
+}
